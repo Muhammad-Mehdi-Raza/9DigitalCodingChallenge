6ed91c07fe0f5f2a84b936a33fc23cd8ac2d0377
Muhammad Mehdi Raza <muhammadmehdiraza@outlook.com>
2021-10-01 06:05:26 +1000

    Updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var AWS = require("aws-sdk");
-AWS.config.update({
-  region: "us-east-1",
-});
+var AWS = require("aws-sdk"); 
+AWS.config.update({ 
+  region: "us-east-1", 
+}); 
 
 exports.handler = async (event) => {
 
@@ -12,48 +12,35 @@ exports.handler = async (event) => {
 
 
     try {
-            //try to parse input JSON to check validity
-            var parsedInput = JSON.parse(input);
+        
+        //try to parse input JSON to check validity
+        var parsedInput = JSON.parse(input);
 
-            console.log("Valid JSON... Continue Processing.");
+        console.log("Valid JSON... Continue Processing.");
 
-            // Handle non-exception-throwing cases like JSON.parse(1234)
-            if (parsedInput && typeof parsedInput === "object") {
-
-                //if parsed successfully, getting out the array of shows
-                var shows = parsedInput.payload;
-
-                //filtering shows based on the required criteria
-                shows.forEach(show => {
-                    if (show.drm && show.episodeCount > 0)
-                    {
-                        //getting required fields from the object
-                        filteredShows.push({image: show.image.showImage, slug: show.slug, title: show.title});
-                    }
-                });
-
-                //preparing response key
-                const returnedShows = {
-                    'response' : filteredShows
-                };
-
-                //setting statusCode and response for success
-                responseCode = 200;
-                responseValue = returnedShows;
-            }
-            else {
-                //Handling special cases like JSON.parse(null)
-                const error = {
-                   'error' : 'Could not decode request: JSON parsing failed'
-                  };
-
-                //setting statusCode and response for error case
-                responseCode = 400;
-                responseValue = error;
-            }
+            //if parsed successfully, getting out the array of shows
+            var shows = parsedInput.payload;
+        
+            //filtering shows based on the required criteria
+            shows.forEach(show => {
+                if (show.drm && show.episodeCount > 0)
+                {
+                    //getting required fields from the object
+                    filteredShows.push({image: show.image.showImage, slug: show.slug, title: show.title});
+                }
+            });
+        
+            //preparing response key
+            const returnedShows = {
+                'response' : filteredShows
+            };
+            
+            //setting statusCode and response for success
+            responseCode = 200;
+            responseValue = returnedShows;
     }
     catch (e) {
-
+        
         //preparing error key
         const error = {
             'error' : 'Could not decode request: JSON parsing failed'
@@ -64,7 +51,7 @@ exports.handler = async (event) => {
         responseValue = error;
     }
     finally{
-
+        
         //preparing response object with appropriate statusCode and responseValue
         const response = {
             statusCode: responseCode,
@@ -75,7 +62,7 @@ exports.handler = async (event) => {
             },
             body: JSON.stringify(responseValue),
         };
-
+        
         //sending out response
         return response;
     }
